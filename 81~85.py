d840233506bfe18a4af793edeab4f39f823b26d6
kohei-irohasu <yktmd059@icloud.com>
2024-02-04 12:22:24 +0900

    commit

--- 81~85.py
+++ 81~85.py
@@ -44,3 +44,37 @@ for i in range(n):
     ans += abs(a[i] - b[i])
 
 print(ans)
+
+
+# 084
+a, b, c = map(int, input().split())
+if c - a - b > 0:
+    if 4 * a * b < (c - a - b) ** 2:
+        print('Yes')
+    else:
+        print('No')
+else:
+    print('No')
+
+
+# 085
+n, x, y = map(int, input().split())
+
+if x > n * 4 or y > n ** 4:
+    print('No')
+else:
+    d = []
+    for i in range(1, n + 1):
+        if y % i == 0:
+            d.append(i)
+    
+    divcnt = len(d)
+    flag = False
+    for i in range(0, divcnt):
+        for j in range(i, divcnt):
+            for k in range(j, divcnt):
+                for l in range(k, divcnt):
+                    if d[i] + d[j] + d[k] + d[l] == x and d[i] * d[j] * d[k] * d[l] == y:
+                        flag = True
+                        
+    print('Yes' if flag else 'No')
